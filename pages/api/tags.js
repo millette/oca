b5c56f68e5a1bdcc4380b4b79372f4572d52d0cd
Robin Millette <robin@millette.info>
2019-10-30 03:39:49 -0400

    Limit number of tags.

--- pages/api/tags.js
+++ pages/api/tags.js
@@ -7,13 +7,15 @@ for (const r in metadata) {
     metadata[r].subject.forEach((s) => tags.set(s, (tags.get(s) || 0) + 1))
 }
 
-const tagsArray = Array.from(tags)
-  .filter((a) => a[1] >= 2)
-  .sort((a, b) => {
-    const av = a[1]
-    const bv = b[1]
-    if (av > bv) return -1
-    if (av < bv) return 1
-  })
+const tagsArray = Array.from(tags).sort((a, b) => {
+  const av = a[1]
+  const bv = b[1]
+  if (av > bv) return -1
+  if (av < bv) return 1
+})
 
-export default (req, res) => res.status(200).json(tagsArray)
+export default (req, res) => {
+  const n = req.query.n || 50
+  const f = tagsArray.filter((a) => a[1] > n)
+  res.status(200).json(f)
+}
