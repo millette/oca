ebd211e90b84cb425de95b6668d71f65ac10b0e8
Robin Millette <robin@millette.info>
2019-10-30 00:46:09 -0400

    Fetch proper svg by key.

--- pages/key.js
+++ pages/key.js
@@ -1,31 +1,37 @@
 // npm
+import Link from "next/link"
 import "isomorphic-unfetch"
 
 const Page = ({ k, d }) => (
-  <div style={{ display: "flex" }}>
-    <img src={"/api/show-one-key?key=" + k} />
-    <div>
-      <h1>{d.title}</h1>
-      <h2>{d.creator}</h2>
-      {d.description && <p>{d.description}</p>}
-      {d.subject && d.subject.length && (
-        <ul>
-          {d.subject.map((subject) => (
-            <li key={subject}>{subject}</li>
-          ))}
-        </ul>
-      )}
-      <pre>{JSON.stringify(d, null, 2)}</pre>
+  <div>
+    <Link href="/all">
+      <a>Show List</a>
+    </Link>
+
+    <div style={{ display: "flex" }}>
+      <img src={"/api/show-one-key?key=" + k} />
+      <div>
+        <h1>{d.title}</h1>
+        <h2>{d.creator}</h2>
+        {d.description && <p>{d.description}</p>}
+        {d.subject && d.subject.length && (
+          <ul>
+            {d.subject.map((subject) => (
+              <li key={subject}>{subject}</li>
+            ))}
+          </ul>
+        )}
+        <pre>{JSON.stringify(d, null, 2)}</pre>
+      </div>
     </div>
   </div>
 )
 
 Page.getInitialProps = async (o) => {
-  const u =
-    o.req && new URL("http://localhost" + o.req.url).searchParams.get("key")
-  const res = await fetch("http://localhost:3000/api/one-key?key=" + u)
+  const k = o.query.key
+  const res = await fetch("http://localhost:3000/api/one-key?key=" + k)
   const d = await res.json()
-  return { k: u, d }
+  return { k, d }
 }
 
 export default Page
