ba35f646757379f681fbafb30793809914ef7262
Robin Millette <robin@millette.info>
2019-10-30 12:20:46 -0400

    Show all tags.

---
+++ pages/tags.js
@@ -0,0 +1,32 @@
+// npm
+import Link from "next/link"
+import "isomorphic-unfetch"
+
+const Tags = ({ t }) => (
+  <div>
+    <ol>
+      {t.map(([k, n]) => (
+        <li key={k}>
+          <Link
+            href={{ pathname: "/tagged", query: { tag: k } }}
+            as={`/tagged?tag=${k}`}
+          >
+            <a>
+              {k} ({n})
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ol>
+  </div>
+)
+
+Tags.getInitialProps = async (o) => {
+  const n = o.query.n || 50
+
+  const res = await fetch("http://localhost:3000/api/tags?n=" + n)
+  const t = await res.json()
+  return { t }
+}
+
+export default Tags
