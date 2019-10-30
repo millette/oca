7c28ae67a51898ced94add36b0f5d3521f5da06b
Robin Millette <robin@millette.info>
2019-10-29 23:42:20 -0400

    Catch error.

--- pages/api/show-one.js
+++ pages/api/show-one.js
@@ -1,4 +1,5 @@
 import fs from "fs"
+import { pipeline } from "stream"
 
 const cnt = fs.createReadStream(
   "clipart/animals/2_dead_frogs_lumen_desig_01.svg",
@@ -7,5 +8,8 @@ const cnt = fs.createReadStream(
 
 export default (req, res) => {
   res.status(200).setHeader("Content-Type", "image/svg+xml")
-  cnt.pipe(res)
+  // cnt.pipe(res)
+  pipeline(cnt, res, (err) => {
+    if (err) console.error("Oupsy", err)
+  })
 }
