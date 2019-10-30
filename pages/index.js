e73be919a09423ca5402f328a7ad70f7c7854eee
Robin Millette <robin@millette.info>
2019-10-30 00:01:12 -0400

    Add demo page.

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+// npm
+import "isomorphic-unfetch"
+
+//
+
+const Page = ({ d }) => (
+  <div style={{ display: "flex" }}>
+    <img src="/api/show-one" />
+    <div>
+      <h1>{d.title}</h1>
+      <h2>{d.creator}</h2>
+      {d.description && <p>{d.description}</p>}
+      {d.subject && d.subject.length && (
+        <ul>
+          {d.subject.map((subject) => (
+            <li key={subject}>{subject}</li>
+          ))}
+        </ul>
+      )}
+      <pre>{JSON.stringify(d, null, 2)}</pre>
+    </div>
+  </div>
+)
+
+Page.getInitialProps = async (o) => {
+  const res = await fetch("http://localhost:3000/api/one")
+  const d = await res.json()
+  return { d }
+}
+
+export default Page
