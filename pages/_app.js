0c58c039d2d57641e20fef64cb3676589a67f971
Robin Millette <robin@millette.info>
2019-11-01 01:32:40 -0400

    Add layout to app.

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,31 @@
 // npm
 import App from "next/app"
 import Link from "next/link"
-import { jsx, ThemeProvider, Styled, ColorMode } from "theme-ui"
+import {
+  jsx,
+  Layout,
+  ThemeProvider,
+  Styled,
+  ColorMode,
+  Header,
+  Main,
+  Container,
+  Footer,
+} from "theme-ui"
 import { roboto as theme } from "@theme-ui/presets"
 
+theme.styles.header = {
+  display: "block",
+}
+
 const components = {
+  /*
   a: ({ children, href }) => (
     <Link href={href} passHref>
       <Styled.a>{children}</Styled.a>
     </Link>
   ),
+  */
 }
 
 class MyApp extends App {
@@ -21,7 +37,25 @@ class MyApp extends App {
       <ThemeProvider components={components} theme={theme}>
         <ColorMode />
         <Styled.root>
-          <Component {...pageProps} />
+          <Layout>
+            <Header>
+              <Container>
+                <Styled.h1>OpenClipArt</Styled.h1>
+                <Styled.h2>A work in progress</Styled.h2>
+              </Container>
+            </Header>
+
+            <Main>
+              <Container>
+                <Component {...pageProps} />
+              </Container>
+            </Main>
+            <Footer>
+              <Container>
+                <Styled.p>Footer stuff</Styled.p>
+              </Container>
+            </Footer>
+          </Layout>
         </Styled.root>
       </ThemeProvider>
     )
