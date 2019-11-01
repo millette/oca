e4ae0afff3462b9a08c2b17422317f64d9ed1a96
Robin Millette <robin@millette.info>
2019-11-01 07:16:16 -0400

    Show sample of all SVGs on frontpage.

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,50 @@
 // npm
 import Link from "next/link"
 import fetcher from "../lib/fetcher"
-import { jsx, Styled } from "theme-ui"
+import { jsx, Styled, Flex, Box } from "theme-ui"
 
-const Tags = ({ t }) => (
+// self
+import Svg from "../components/svg"
+
+const Tags = ({ t, t2 }) => (
   <>
     <Styled.h3>All tags</Styled.h3>
-    <Styled.ol>
-      {t.map(([k, n]) => (
-        <Styled.li key={k}>
-          <Link
-            passHref
-            href={{ pathname: "/tagged", query: { tag: k } }}
-            as={`/tagged?tag=${k}`}
-          >
-            <Styled.a>
-              {k} ({n})
-            </Styled.a>
-          </Link>
-        </Styled.li>
-      ))}
-    </Styled.ol>
+    <Flex sx={{ flexWrap: "wrap" }}>
+      <Box sx={{ width: ["100%", "50%"] }}>
+        <Styled.ol>
+          {t.map(([k, n]) => (
+            <Styled.li key={k}>
+              <Link
+                passHref
+                href={{ pathname: "/tagged", query: { tag: k } }}
+                as={`/tagged?tag=${k}`}
+              >
+                <Styled.a>
+                  {k} ({n})
+                </Styled.a>
+              </Link>
+            </Styled.li>
+          ))}
+        </Styled.ol>
+      </Box>
+
+      <Box sx={{ width: ["100%", "50%"] }}>
+        <Flex sx={{ maxHeight: 200, flexWrap: "wrap" }}>
+          {t2.map(({ svg, fn }) => (
+            <Box
+              key={fn}
+              sx={{ p: 16, overflow: "hidden", width: ["100%", "33.333%"] }}
+            >
+              <Link href={{ pathname: "/key", query: { key: fn } }}>
+                <a>
+                  <Svg svg={svg} />
+                </a>
+              </Link>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </Flex>
   </>
 )
 
@@ -30,7 +54,11 @@ Tags.getInitialProps = async (o) => {
   const n = o.query.n || 50
   const res = await fetcher(o.req, "http://localhost:3000/api/tags?n=" + n)
   const t = await res.json()
-  return { t }
+
+  const res2 = await fetcher(o.req, "http://localhost:3000/api/sample")
+  const t2 = await res2.json()
+
+  return { t, t2 }
 }
 
 export default Tags
