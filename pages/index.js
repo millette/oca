1b1c927e939d3da163bb14e86211aea7c143ca70
Robin Millette <robin@millette.info>
2019-11-01 01:32:54 -0400

    Simplify a bit.

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,21 @@ import fetcher from "../lib/fetcher"
 import { jsx, Styled } from "theme-ui"
 
 const Tags = ({ t }) => (
-  <div>
-    <Styled.ol>
-      {t.map(([k, n]) => (
-        <Styled.li key={k}>
-          <Link
-            passHref
-            href={{ pathname: "/tagged", query: { tag: k } }}
-            as={`/tagged?tag=${k}`}
-          >
-            <Styled.a>
-              {k} ({n})
-            </Styled.a>
-          </Link>
-        </Styled.li>
-      ))}
-    </Styled.ol>
-  </div>
+  <Styled.ol>
+    {t.map(([k, n]) => (
+      <Styled.li key={k}>
+        <Link
+          passHref
+          href={{ pathname: "/tagged", query: { tag: k } }}
+          as={`/tagged?tag=${k}`}
+        >
+          <Styled.a>
+            {k} ({n})
+          </Styled.a>
+        </Link>
+      </Styled.li>
+    ))}
+  </Styled.ol>
 )
 
 Tags.getInitialProps = async (o) => {
