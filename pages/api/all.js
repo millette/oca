ab9ead7ca8724c7a7d4c2ab8ed4fab2b334f14ad
Robin Millette <robin@millette.info>
2019-11-02 00:53:27 -0400

    Remove unused files.

--- pages/api/all.js
+++
@@ -1,3 +0,0 @@
-import metadata from "../../metadata.json"
-
-export default (req, res) => res.status(200).json(metadata)
--- pages/api/prefixed-keys.js
+++
@@ -1,8 +0,0 @@
-import metadata from "../../metadata.json"
-
-export default (req, res) => {
-  const p = req.query.prefix
-  const keys = Object.keys(metadata).filter((x) => x.startsWith(p))
-
-  res.status(200).json(keys)
-}
