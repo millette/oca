9d590831ccd14e689147a2ab30906e44c3266fc4
Robin Millette <robin@millette.info>
2019-11-02 05:18:05 -0400

    Handle svg by key too.

--- components/svg.js
+++ components/svg.js
@@ -1,3 +1,17 @@
-const Svg = ({ svg: __html }) => <div dangerouslySetInnerHTML={{ __html }} />
+// npm
+import { useState, useEffect } from "react"
+
+const Svg = ({ k, svg }) => {
+  const [__html, setA] = useState(svg || "")
+
+  useEffect(() => {
+    if (svg || !k) return
+    fetch("/api/opt-one-key?key=computer/icons/flat-theme/action/clipart.svg")
+      .then((res) => res.text())
+      .then(setA)
+  }, [])
+
+  return <div dangerouslySetInnerHTML={{ __html }} />
+}
 
 export default Svg
