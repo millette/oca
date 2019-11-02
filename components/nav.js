0c99a5d07b97436340ae0af842056d33a2245e05
Robin Millette <robin@millette.info>
2019-11-02 05:18:47 -0400

    Add nav component.

---
+++ components/nav.js
@@ -0,0 +1,20 @@
+/** @jsx jsx */
+
+// npm
+import { jsx, Flex, Box } from "theme-ui"
+
+// self
+import NavLink from "./nav-link"
+
+const Nav = () => {
+  return (
+    <Flex sx={{ alignItems: "center" }}>
+      <NavLink href="/">Front</NavLink>
+      <NavLink href="/tags">Tags</NavLink>
+      <Box sx={{ mx: "auto" }}></Box>
+      <NavLink href="/about">About</NavLink>
+    </Flex>
+  )
+}
+
+export default Nav
