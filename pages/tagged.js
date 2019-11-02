6448043a4ebe819e726940e33f223b98563bd7bb
Robin Millette <robin@millette.info>
2019-11-01 21:48:09 -0400

    Show random sample of SVGs tagged with.

--- pages/tagged.js
+++ pages/tagged.js
@@ -3,9 +3,12 @@
 // npm
 import Link from "next/link"
 import fetcher from "../lib/fetcher"
-import { jsx, Styled } from "theme-ui"
+import { jsx, Styled, Flex, Box } from "theme-ui"
 
-const Tagged = ({ d, tag }) => (
+// self
+import Sample from "../components/sample"
+
+const Tagged = ({ d, tag, t2 }) => (
   <>
     <Styled.h3>
       <Link passHref href="/">
@@ -13,19 +16,27 @@ const Tagged = ({ d, tag }) => (
       </Link>
     </Styled.h3>
     <Styled.h4>Tagged with {tag}</Styled.h4>
-    <Styled.ol>
-      {d.map((key) => (
-        <Styled.li key={key}>
-          <Link
-            passHref
-            href={{ pathname: "/key", query: { key } }}
-            as={`/key?key=${key}`}
-          >
-            <Styled.a>{key}</Styled.a>
-          </Link>
-        </Styled.li>
-      ))}
-    </Styled.ol>
+
+    <Flex sx={{ flexWrap: "wrap" }}>
+      <Box sx={{ width: ["100%", "50%"] }}>
+        <Styled.ol>
+          {d.map((key) => (
+            <Styled.li key={key}>
+              <Link
+                passHref
+                href={{ pathname: "/key", query: { key } }}
+                as={`/key?key=${key}`}
+              >
+                <Styled.a>{key}</Styled.a>
+              </Link>
+            </Styled.li>
+          ))}
+        </Styled.ol>
+      </Box>
+      <Box sx={{ width: ["100%", "50%"] }}>
+        <Sample data={t2} />
+      </Box>
+    </Flex>
   </>
 )
 
@@ -36,7 +47,14 @@ Tagged.getInitialProps = async (o) => {
     "http://localhost:3000/api/tagged-keys?tag=" + tag
   )
   const d = await res.json()
-  return { tag, d }
+
+  const res2 = await fetcher(
+    o.req,
+    "http://localhost:3000/api/sample?tag=" + tag
+  )
+  const t2 = await res2.json()
+
+  return { tag, d, t2 }
 }
 
 export default Tagged
