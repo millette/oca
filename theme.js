acc3685797539af1df40321ec60cc898398e9a36
Robin Millette <robin@millette.info>
2019-11-04 18:25:58 -0500

    Change breakpoints.

--- theme.js
+++ theme.js
@@ -5,6 +5,6 @@ import { tosh as theme } from "@theme-ui/presets"
 // "base", "dark", "deep", "funk", "future", "roboto"
 // "swiss", "system", "bootstrap", "bulma", "tailwind"
 
-if (!theme.breakpoints) theme.breakpoints = ["40em", "56em", "64em"]
+if (!theme.breakpoints) theme.breakpoints = ["44em", "56em", "64em"]
 
 export default theme
