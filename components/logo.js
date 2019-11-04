b62beebcf0076f729723b06cdb7a560a4918eaf2
Robin Millette <robin@millette.info>
2019-11-03 19:35:39 -0500

    Add generated logo (from svg).

---
+++ components/logo.js
@@ -0,0 +1,50 @@
+const SvgClipart = (props) => (
+  <svg viewBox="0 0 60 60" {...props}>
+    <rect
+      height={52.141}
+      rx={1.913}
+      transform="rotate(90)"
+      width={52.141}
+      x={4.328}
+      y={-56.211}
+      fill="#b3b3b3"
+      fillRule="evenodd"
+    />
+    <path
+      d="M50.198 55.927c-1.953-6.07-5.127-11.94-10.322-15.165-5.23-.227-11.815 0-18.181 0-5.639 3.001-9.063 8.963-11.195 15.165"
+      fontSize={12}
+      fill="#666"
+      fillRule="evenodd"
+      stroke="#333"
+      strokeWidth={2.647}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M45.98 28.934a16.145 16.145 0 10-32.29 0 16.145 16.145 0 1032.29 0H29.835z"
+      fontSize={12}
+      fill="#ccc"
+      fillRule="evenodd"
+      stroke="#333"
+      strokeWidth={2.64708559}
+      strokeLinejoin="round"
+      strokeLinecap="round"
+    />
+    <rect
+      height={52.141}
+      rx={1.913}
+      transform="rotate(90)"
+      width={52.141}
+      x={4.328}
+      y={-56.211}
+      fill="none"
+      stroke="#333"
+      strokeWidth={3.125}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeDasharray="3.125 3.125"
+    />
+  </svg>
+)
+
+export default SvgClipart
