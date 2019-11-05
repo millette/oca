6cf10a3a1915eeec15b33e474e4436777d6c1a53
Robin Millette <robin@millette.info>
2019-11-05 00:59:26 -0500

    Default to prefered color mode.

--- theme.js
+++ theme.js
@@ -6,5 +6,6 @@ import { tosh as theme } from "@theme-ui/presets"
 // "swiss", "system", "bootstrap", "bulma", "tailwind"
 
 if (!theme.breakpoints) theme.breakpoints = ["44em", "56em", "64em"]
+theme.useColorSchemeMediaQuery = true
 
 export default theme
