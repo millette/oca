9c6e995909b61a59743846f3d269681733a90f0d
Robin Millette <robin@millette.info>
2019-11-05 08:15:04 -0500

    UX to force white background on clipart.

--- pages/key.js
+++ pages/key.js
@@ -3,6 +3,7 @@
 // npm
 import Link from "next/link"
 import Head from "next/head"
+import { useState } from "react"
 import fetcher from "../lib/fetcher"
 import { jsx, Styled, Flex, Box } from "theme-ui"
 import filesize from "filesize"
@@ -12,71 +13,101 @@ const size = filesize.partial({ standard: "iec" })
 // self
 import Svg from "../components/svg"
 
-const Key = ({ d, svg, sizes, k }) => (
-  <Flex sx={{ flexWrap: "wrap" }}>
-    <Head>
-      <title>{[d.title, d.creator].join(" by ")} - OpenClipArts Explorer</title>
-    </Head>
+// const forceWhite = undefined // "#fff"
 
-    <Box sx={{ overflowX: "hidden", width: ["100%", "50%"] }}>
-      <Svg svg={svg} />
-    </Box>
-    <Box sx={{ pl: "1rem", width: ["100%", "50%"] }}>
-      <Styled.h1>{d.title}</Styled.h1>
-      <Styled.h2>by {d.creator}</Styled.h2>
-      {d.description && <Styled.p>{d.description}</Styled.p>}
-      <Styled.table>
-        <tbody>
-          <Styled.tr>
-            <Styled.th>Original</Styled.th>
-            <Styled.td sx={{ fontStyle: "italic" }}>with metadata</Styled.td>
-            <Styled.td>
-              <Styled.a href={`/api/show-one-key?key=${k}`} download>
-                download
-              </Styled.a>
-            </Styled.td>
-            <Styled.td style={{ textAlign: "right" }}>
-              {size(sizes.original)}
-            </Styled.td>
-          </Styled.tr>
-          <Styled.tr>
-            <Styled.th>Optimized</Styled.th>
-            <Styled.td sx={{ fontStyle: "italic" }}>as shown</Styled.td>
-            <Styled.td>
-              <Styled.a href={`/api/opt-one-key?key=${k}`} download>
-                download
-              </Styled.a>
-            </Styled.td>
-            <Styled.td style={{ textAlign: "right" }}>
-              {size(sizes.optimized)}
-            </Styled.td>
-          </Styled.tr>
-          <Styled.tr>
-            <Styled.th>Ratio</Styled.th>
-            <Styled.td colSpan={2} sx={{ fontStyle: "italic" }}>
-              saving
-            </Styled.td>
-            <Styled.td style={{ textAlign: "right" }}>
-              {100 - Math.round((100 * sizes.optimized) / sizes.original)}%
-            </Styled.td>
-          </Styled.tr>
-        </tbody>
-      </Styled.table>
+const white = "#fff"
 
-      {d.subject && d.subject.length && (
-        <Styled.ul>
-          {d.subject.map((tag) => (
-            <Styled.li key={tag}>
-              <Link passHref href={{ pathname: "/tagged", query: { tag } }}>
-                <Styled.a>{tag}</Styled.a>
-              </Link>
-            </Styled.li>
-          ))}
-        </Styled.ul>
-      )}
-    </Box>
-  </Flex>
-)
+const Key = ({ d, svg, sizes, k }) => {
+  const [forceWhite, setForceWhite] = useState({})
+  // const [forced, setForced] = useState()
+
+  const handleEnter = () => !forceWhite.forced && setForceWhite({ white })
+
+  const handleLeave = () => !forceWhite.forced && setForceWhite({})
+
+  const handleForce = () =>
+    !forceWhite.forced && setForceWhite({ white, forced: true })
+
+  return (
+    <Flex sx={{ flexWrap: "wrap" }}>
+      <Head>
+        <title>
+          {[d.title, d.creator].join(" by ")} - OpenClipArts Explorer
+        </title>
+      </Head>
+
+      <Box
+        style={{ background: forceWhite.white }}
+        sx={{ textAlign: "right", overflowX: "hidden", width: ["100%", "50%"] }}
+      >
+        <Styled.div
+          title="Click to force a white background"
+          as="button"
+          onClick={handleForce}
+          onMouseEnter={handleEnter}
+          onMouseLeave={handleLeave}
+        >
+          White background
+        </Styled.div>
+        <Svg style={{ padding: "0.5rem" }} svg={svg} />
+      </Box>
+      <Box sx={{ pl: "1rem", width: ["100%", "50%"] }}>
+        <Styled.h1>{d.title}</Styled.h1>
+        <Styled.h2>by {d.creator}</Styled.h2>
+        {d.description && <Styled.p>{d.description}</Styled.p>}
+        <Styled.table>
+          <tbody>
+            <Styled.tr>
+              <Styled.th>Original</Styled.th>
+              <Styled.td sx={{ fontStyle: "italic" }}>with metadata</Styled.td>
+              <Styled.td>
+                <Styled.a href={`/api/show-one-key?key=${k}`} download>
+                  download
+                </Styled.a>
+              </Styled.td>
+              <Styled.td style={{ textAlign: "right" }}>
+                {size(sizes.original)}
+              </Styled.td>
+            </Styled.tr>
+            <Styled.tr>
+              <Styled.th>Optimized</Styled.th>
+              <Styled.td sx={{ fontStyle: "italic" }}>as shown</Styled.td>
+              <Styled.td>
+                <Styled.a href={`/api/opt-one-key?key=${k}`} download>
+                  download
+                </Styled.a>
+              </Styled.td>
+              <Styled.td style={{ textAlign: "right" }}>
+                {size(sizes.optimized)}
+              </Styled.td>
+            </Styled.tr>
+            <Styled.tr>
+              <Styled.th>Ratio</Styled.th>
+              <Styled.td colSpan={2} sx={{ fontStyle: "italic" }}>
+                saving
+              </Styled.td>
+              <Styled.td style={{ textAlign: "right" }}>
+                {100 - Math.round((100 * sizes.optimized) / sizes.original)}%
+              </Styled.td>
+            </Styled.tr>
+          </tbody>
+        </Styled.table>
+
+        {d.subject && d.subject.length && (
+          <Styled.ul>
+            {d.subject.map((tag) => (
+              <Styled.li key={tag}>
+                <Link passHref href={{ pathname: "/tagged", query: { tag } }}>
+                  <Styled.a>{tag}</Styled.a>
+                </Link>
+              </Styled.li>
+            ))}
+          </Styled.ul>
+        )}
+      </Box>
+    </Flex>
+  )
+}
 
 Key.getInitialProps = async (o) => {
   const { key } = o.query
