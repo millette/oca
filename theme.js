fdcdbbbffad754e9a9b0c04d833f00e50d4685c3
Robin Millette <robin@millette.info>
2019-11-05 00:26:30 -0500

    Simplify theme (use default breakpoints).

--- theme.js
+++ theme.js
@@ -5,6 +5,12 @@ import { tosh as theme } from "@theme-ui/presets"
 // "base", "dark", "deep", "funk", "future", "roboto"
 // "swiss", "system", "bootstrap", "bulma", "tailwind"
 
-if (!theme.breakpoints) theme.breakpoints = ["44em", "56em", "64em"]
+// There's actually a default...
+// if (!theme.breakpoints) theme.breakpoints = ["44em", "56em", "64em"]
 
+// The following does nothing it seems...
+// if (!theme.styles.summary) theme.styles.summary = {}
+// theme.styles.summary.cursor = "help"
+
+console.log("theme", JSON.stringify(theme, null, 2))
 export default theme
