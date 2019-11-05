f7436a4dd7964421f620f408244b2e2b0f13ecf4
Robin Millette <robin@millette.info>
2019-11-05 04:56:37 -0500

    Improve deps api route.

--- pages/api/deps.js
+++ pages/api/deps.js
@@ -1,29 +1,56 @@
-// npm
-import readPkg from "read-pkg"
+// core
+import { dirname } from "path"
 
-export default (req, res) => {
-  // console.log('API', req.query)
+// npm
+import readPkg from "read-pkg-up"
+
+const okRe = /^[\d@a-z/.-]+$/
+const depOk = (d) => {
+  if (!d) return
+  if (okRe.test(d)) return true
+  const err = new Error("Invalid module name")
+  err.given = JSON.stringify(`${d}`)
+  throw err
+}
+
+const cleanPkg = ({ packageJson: json, path }, full) => {
+  const ppp = {}
+  for (const r in json) {
+    if (!r.indexOf("_")) continue
+    if (r.endsWith("ependencies")) {
+      ppp[r] = Object.keys(json[r]).sort()
+    } else {
+      ppp[r] = json[r]
+    }
+  }
+
+  if (full) {
+    const { dependencies, ...pkg } = ppp
+    return { dependencies, pkg, path, full }
+  }
+  const { dependencies, ...pkg2 } = ppp
+  const pkg = {
+    name: pkg2.name,
+    version: pkg2.version,
+    description: pkg2.description,
+    license: pkg2.license,
+    engines: pkg2.engines,
+    homepage: pkg2.homepage,
+    repository: pkg2.repository,
+    author: pkg2.author,
+  }
+  return { dependencies, pkg, path, full }
+}
+
+export default async (req, res) => {
   const { dep } = req.query
 
   try {
     // battling webpack
-    const p = dep && eval(`require.resolve("${dep}")`)
-
-    const ret = { dep, p }
-    console.log("RET", ret)
-
-    res.status(200).json(ret)
+    const cwd = dirname(depOk(dep) ? eval(`require.resolve("${dep}")`) : ".")
+    const pkg = await readPkg({ cwd })
+    res.status(200).json({ dep, cwd, ...cleanPkg(pkg, req.query.full) })
   } catch (e) {
-    console.log("YIKES", e)
-    res.status(404).send("Not found???")
+    res.status(404).send("Not found??? " + e.message)
   }
 }
-
-/*
-const p = dep || "../.."
-const p2 = [p, "package.json"].join("/")
-
-console.log("PPP", p, dep, p2)
-const { dependencies } = require(p2)
-res.status(200).json(Object.keys(dependencies))
-*/
