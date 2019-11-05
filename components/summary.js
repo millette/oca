ba25e1de4ad7dfa6becab11c68e00e56485b4306
Robin Millette <robin@millette.info>
2019-11-05 00:20:24 -0500

    Space out summary components.

--- components/summary.js
+++ components/summary.js
@@ -11,7 +11,7 @@ const Summary = (props) => <summary {...props} css={css({ cursor: "pointer" })} 
 import { jsx, Styled } from "theme-ui"
 
 const Summary = (props) => (
-  <Styled.div as="summary" {...props} sx={{ cursor: "pointer" }} />
+  <Styled.div as="summary" {...props} sx={{ mt: "1rem", cursor: "pointer" }} />
 )
 
 export default Summary
