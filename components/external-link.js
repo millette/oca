0804b5a42dfb8678e2f07a8767712cbf881725a5
Robin Millette <robin@millette.info>
2019-11-05 07:33:35 -0500

    Style external link.

--- components/external-link.js
+++ components/external-link.js
@@ -3,8 +3,8 @@
 //npm
 import { jsx, Styled } from "theme-ui"
 
-export default ({ children, href }) => (
-  <Styled.a target="_blank" rel="noopener noreferrer" href={href}>
+export default ({ children, style, href }) => (
+  <Styled.a style={style} target="_blank" rel="noopener noreferrer" href={href}>
     {children || href}
   </Styled.a>
 )
