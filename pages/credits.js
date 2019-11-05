5a747ebcbeb4bb0d99b778cace87d5451b9bb38b
Robin Millette <robin@millette.info>
2019-11-05 04:57:30 -0500

    Improve credits page.

--- pages/credits.js
+++ pages/credits.js
@@ -11,6 +11,7 @@ import Dependencies from "../components/deps"
 import Summary from "../components/summary"
 
 const Credits = (props) => {
+  if (props.error) return "Not good: " + props.error
   return (
     <>
       <Head>
@@ -29,14 +30,17 @@ const Credits = (props) => {
 }
 
 Credits.getInitialProps = async (o) => {
-  const { dep } = o.query
+  const { dep, full } = o.query
   try {
-    const res = await fetcher(`api/deps?dep=${dep || ""}`, o.req)
-    const deps = await res.json()
-    return { deps, dep }
+    const res = await fetcher(
+      `api/deps?full=${full || ""}&dep=${dep || ""}`,
+      o.req
+    )
+    if (!res.ok) return { error: "Niet" }
+    return res.json()
   } catch (e) {
     console.log("OUCH", e)
-    return {}
+    return { error: e.message }
   }
 }
 
