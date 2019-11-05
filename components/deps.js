acb31a46914cca22422ca784aba6e63991907eb1
Robin Millette <robin@millette.info>
2019-11-05 04:58:02 -0500

    Improve credits component.

--- components/deps.js
+++ components/deps.js
@@ -1,11 +1,51 @@
 /** @jsx jsx */
 
 // npm
-import { jsx, Styled } from "theme-ui"
+import Link from "next/link"
+import { jsx, Styled, Flex, Box } from "theme-ui"
 
-const Deps = (props) => (
+const Deps = ({ deps }) => (
   <>
-    <Styled.pre>{JSON.stringify(props, null, 2)}</Styled.pre>
+    <Flex>
+      <Box sx={{ pr: 4, width: ["100%", "50%"] }}>
+        <Styled.h5>
+          {deps.pkg.name} ({deps.pkg.version}) {deps.pkg.license}
+        </Styled.h5>
+        {deps.pkg.description && <Styled.p>{deps.pkg.description}</Styled.p>}
+        {deps.pkg.homepage && <Styled.p>{deps.pkg.homepage}</Styled.p>}
+        {deps.pkg.author && (
+          <Styled.pre>{JSON.stringify(deps.pkg.author, null, 2)}</Styled.pre>
+        )}
+        {deps.pkg.repository && (
+          <Styled.pre>
+            {JSON.stringify(deps.pkg.repository, null, 2)}
+          </Styled.pre>
+        )}
+      </Box>
+      <Box sx={{ width: ["100%", "50%"] }}>
+        {deps.dependencies ? (
+          <>
+            <Styled.h5>Dependencies</Styled.h5>
+
+            <Styled.ul sx={{ display: "flex", flexWrap: "wrap" }}>
+              {deps.dependencies.map((d) => (
+                <Styled.li key={d} sx={{ mx: 2, listStyle: "none" }}>
+                  <Link href={`/credits?dep=${d}`} passHref>
+                    <Styled.a>{d}</Styled.a>
+                  </Link>
+                </Styled.li>
+              ))}
+            </Styled.ul>
+          </>
+        ) : (
+          <Styled.h5>
+            <Styled.code>{deps.pkg.name}</Styled.code> has no dependencies
+          </Styled.h5>
+        )}
+      </Box>
+    </Flex>
+
+    <Styled.pre>{JSON.stringify(deps, null, 2)}</Styled.pre>
   </>
 )
 
