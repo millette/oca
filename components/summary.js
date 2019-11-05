e29fc4d5b876ed7f9dea7ca09da81fd8b74372f8
Robin Millette <robin@millette.info>
2019-11-05 00:03:04 -0500

    Add styled summary component (to add pointer cursor).

---
+++ components/summary.js
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+
+/*
+I was using the following until I noticed
+the Styled.div could be used as wildcard
+
+import { jsx, css } from "theme-ui"
+const Summary = (props) => <summary {...props} css={css({ cursor: "pointer" })} />
+*/
+
+import { jsx, Styled } from "theme-ui"
+
+const Summary = (props) => (
+  <Styled.div as="summary" {...props} sx={{ cursor: "pointer" }} />
+)
+
+export default Summary
