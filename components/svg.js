eb787a028cfbdccbbe082a8c6d1750531234f0a3
Robin Millette <robin@millette.info>
2019-11-06 01:06:32 -0500

    Fix svg component (was hardcoded).

--- components/svg.js
+++ components/svg.js
@@ -2,16 +2,21 @@
 import { useState, useEffect } from "react"
 
 const Svg = ({ style, k, svg }) => {
-  const [__html, setA] = useState(svg || "")
+  const [content, setA] = useState(svg || "")
 
   useEffect(() => {
     if (svg || !k) return
-    fetch("/api/opt-one-key?key=computer/icons/flat-theme/action/clipart.svg")
+    fetch(`/api/opt-one-key?key=${k}`)
       .then((res) => res.text())
       .then(setA)
   }, [])
 
-  return <div style={style} dangerouslySetInnerHTML={{ __html }} />
+  return (
+    <div
+      style={style}
+      dangerouslySetInnerHTML={{ __html: content || "Loading..." }}
+    />
+  )
 }
 
 export default Svg
