a5e05aa3bb5a3b19144312ec9f43856c817a2809
Robin Millette <robin@millette.info>
2019-11-06 05:02:42 -0500

    Rename 'front' to 'frontpage'.

--- components/nav.js
+++ components/nav.js
@@ -9,7 +9,7 @@ import NavLink from "./nav-link"
 const Nav = () => {
   return (
     <Flex sx={{ alignItems: "center" }}>
-      <NavLink href="/">Front</NavLink>
+      <NavLink href="/">Frontpage</NavLink>
       <NavLink href="/search">Search</NavLink>
       <NavLink href="/tags">Browse</NavLink>
       <Box sx={{ mx: "auto" }}></Box>
